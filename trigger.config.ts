ef3371322e67dc3ed6daeb71e851b61176e4ee26
Anthony Cueva <hi@cueva.io>
2025-05-26 05:20:26 -0500

    fix: change runtime from bun to node in trigger configuration

--- trigger.config.ts
+++ trigger.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
 	project: "proj_wrimtjivrzxcqcazalxf",
-	runtime: "bun",
+	runtime: "node",
 	logLevel: "log",
 	// The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
 	// You can override this on an individual task.
